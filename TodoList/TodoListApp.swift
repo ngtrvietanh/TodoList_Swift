5dd3c40bcd65e237849f14726646879ddc2f0998
Trần Việt Anh <tranvietanhcbh>
2024-11-26 14:55:24 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Việt Anh on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
